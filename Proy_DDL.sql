27c7b234fbe9cbeb3b03832404db41ce09db7d02
Acordia2310 <andrescortesdiaz2005@gmail.com>
2025-04-02 12:56:21 +0200

    Modelo teminado y DDL

--- Proy_DDL.sql
+++ Proy_DDL.sql
@@ -2,40 +2,124 @@ drop database if exists proy_seneca;
 create database proy_seneca;
 use proy_seneca;
 
-create table Usuarios(
+create table persona (
 	id int primary key auto_increment,
     nombre varchar(50),
-    apellidos varchar(50),
-    DNI char(9),
-    email varchar(50),
-    tipo enum('Alumno', 'Profesor', 'Tutor', 'Administrativo')
-);
-create table Centro (
-	id int primary key auto_increment,
-	nombre varchar(100),
+    apellidos varchar(100),
     direccion varchar(100),
-    telefono char(9),
-    DNI char(9),
-    tipo enum('Instituto', 'Colegio', 'FP')
+    fecha_nac date,
+    dni char(9),
+    genero enum('Masculino', 'Femenino')
 );
-create table curso(
+
+create table profesor (
 	id int primary key auto_increment,
-    year_inicio date,
-    year_fin date
+    especialidad varchar(100),
+    constraint fk_profesor_persona foreign key (id) references persona(id)
 );
-create table asignatura(
+
+create table alumno(
 	id int primary key auto_increment,
-    nombre varchar(50),
-    codigo int,
-    curso_asignado int,
-    constraint fk_asignatura_curso foreign key (curso_asignado) references curso(id)
+    estado enum('Activo', 'Inactivo')
 );
-create table matricula(
-	id int,
+
+create table faltas_asistencia (
+	id int primary key auto_increment,
     alumno int,
-    asignatura int,
-    estado enum('Aprobado', 'Pendiente'),
-    constraint pk_matricula primary key (id, alumno, asignatura),
+    faltas_justificadas int,
+    faltas_injustificadas int,
+    constraint fk_faltas_asistencias_alumno foreign key (alumno) references alumno(id)
+);
+
+create table ciclo (
+	id int primary key auto_increment,
+    nombre varchar(100),
+    anio year,
+    periodo enum('Trimestral', 'Cuatrimestral', 'Semestral')
+);
+
+create table modulo_profesional (
+	id int primary key auto_increment,
+    ciclo int,
+    nombre varchar(100),
+    tipo enum('Obligatoria', 'Optativa'),
+    constraint fk_modulo_profesional_ciclo foreign key (ciclo) references ciclo(id)
+);
+
+create table imparte (
+	 profesor int,
+     modulo_profesional int,
+     constraint pk_imparte primary key (profesor,modulo_profesional),
+     constraint fk_imparte_profesor foreign key (profesor) references profesor(id),
+     constraint fk_imparte_modulo_profesional foreign key (modulo_profesional) references modulo_profesional(id)
+);
+
+create table ra (
+	id int primary key auto_increment,
+    modulo_profesional int,
+    ponderacion int,
+    descripcion text,
+    observaciones text,
+    constraint fk_ra_modulo_profesional foreign key (modulo_profesional) references modulo_profesional(id)
+);
+
+create table tarea (
+	id int primary key auto_increment,
+    nombre varchar(100),
+    fecha_entrega date
+);
+
+create table criterios_evaluacion (
+	 id int primary key auto_increment,
+     nombre text,
+     ponderacion int,
+     ra int,
+     constraint fk_criterios_evaluacion_ra foreign key (ra) references ra(id)
+);
+
+create table evaluacion (
+     alumno int,
+     tarea int,
+     criterios_evaluacion int,
+     nota int,
+     constraint pk_evaluacion primary key (alumno, tarea, criterios_evaluacion),
+     constraint fk_evaluacion_alumno foreign key (alumno) references alumno(id),
+     constraint fk_evaluacion_tarea foreign key (tarea) references tarea(id),
+     constraint fk_evaluacion_criterios_evolucion foreign key (criterios_evaluacion) references criterios_evaluacion(id)
+);
+
+create table centro (
+	id int primary key auto_increment,
+    nombre varchar(100),
+    direccion varchar(100),
+    localidad varchar(100),
+    provincia varchar(100),
+    tipo enum('Instituto', 'Colegio', 'FP', 'Universidad')
+);
+
+create table oferta_educativa (
+	id int primary key auto_increment,
+    centro int,
+    ciclo int,
+    fecha_inicio date,
+    fecha_fin date,
+    constraint fk_oferta_educativa_centro foreign key (centro) references centro(id),
+    constraint fk_oferta_educativa_ciclo foreign key (ciclo) references ciclo(id)
+);
+
+create table curso (
+	id int primary key auto_increment,
+    oferta_educativa int,
+    tipo enum('Eso', 'Bachiller', 'FP')
+);
+
+create table matricula (
+	alumno int,
+    curso int,
+    num_alum int,
+    anio_inicio date,
+    anio_final date,
+    constraint pk_matricula primary key (alumno, curso),
     constraint fk_matricula_alumno foreign key (alumno) references alumno(id),
-    constraint fk_matricula_asignatura foreign key (asignatura) references asignatura(id)
+    constraint fk_matricula_curso foreign key (curso) references curso(id)
 );
